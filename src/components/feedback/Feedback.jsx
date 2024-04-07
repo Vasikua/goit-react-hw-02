d88c394845da583e67abcd9ffc4c3969ed89fc92
Vasyl Mandik <Vasik_ua@ukr.net>
2024-04-06 21:14:28 -0500

    hw-done

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,20 +1,22 @@
-export default function Feedback({good, bad, neutral}) {
+
+import css from "./Feedback.module.css"
+export default function Feedback({ good, bad, neutral }) {
     
  const total = good + bad + neutral;
     const positivFeedBack = Math.round((good / total) * 100);
     
     return (
-        <ul className="list">
+        <ul className={css.list}>
             {total !== 0 ? (
                 <>
-                    <li className="item">Good: {good}</li>
-                    <li className="item">Neutral: {neutral}</li>
-                    <li className="item">Bad: {bad}</li>
-                    <li className="item">Total: {total}</li>
-                    <li className="item">Positiv: {positivFeedBack}%</li>
+                    <li className = {css.item}>Good: {good}</li>
+                    <li className = {css.item}>Neutral: {neutral}</li>
+                    <li className = {css.item}>Bad: {bad}</li>
+                    <li className = {css.item}>Total: {total}</li>
+                    <li className = {css.item}>Positiv: {positivFeedBack}%</li>
                 </>
             ) : (
-                <li className="item">No feedback yeat</li>
+                <li className = {css.item}>No feedback yeat</li>
             )}
         </ul>
     );
--- src/components/options/Options.jsx
+++ src/components/options/Options.jsx
@@ -1,14 +1,14 @@
- 
+ import  css from "./Options.module.css"
 export default function Options({ updateFeedback,  getInitialValues}) {
     
-         return <>
-            <button onClick={() => updateFeedback("good")} >Good</button>
-            <button onClick={() => updateFeedback("neutral")}>Neutral</button>
-            <button onClick={() => updateFeedback("bad")}>Bad</button>
+         return <div className={css.feedbackContainer}>
+            <button className={css.feedbackBtn} onClick={() => updateFeedback("good")} >Good</button>
+            <button className={css.feedbackBtn} onClick={() => updateFeedback("neutral")}>Neutral</button>
+            <button className={css.feedbackBtn} onClick={() => updateFeedback("bad")}>Bad</button>
              { JSON.stringify(getInitialValues()) !== JSON.stringify({ good: 0, bad: 0, neutral: 0 }) ?
-                 (<button onClick={() => updateFeedback("resetAll")}>Reset</button>)
+                 (<button className={css.feedbackBtn} onClick={() => updateFeedback("resetAll")}>Reset</button>)
                  :
-                 (<button disabled ></button>)
+                 (<button className={css.feedbackOff} ></button>)
              }
-        </>
+        </div>
 }
